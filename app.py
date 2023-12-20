34f1018a40dd44d57106ec1ec5fdb71de45af95e
moez <moez@aiprm.org>
2023-12-21 01:43:50 +0200

    Fix a bug

--- app.py
+++ app.py
@@ -10,8 +10,10 @@ with st.form(key='my_form', border=False):
     input_text = st.text_area("Input Text", height=300)
     convert_button = st.form_submit_button("Convert")
 
+output_text_area = st.empty()
+
 if convert_button:
     output_text = convert_text(input_text)
-    st.session_state['output'] = output_text
-
-output_text = st.text_area("Converted Text", height=300, value=st.session_state.get('output', ''))
+    output_text_area.text_area("Converted Text", height=300, value=output_text, key='output')
+else:
+    output_text_area.text_area("Converted Text", height=300, value=st.session_state.get('output', ''), key='output')
